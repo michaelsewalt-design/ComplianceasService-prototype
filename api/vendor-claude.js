b3d3ff0e54eacf69af0090167d0bc1131f1d1e41
michaelsewalt-design <michael.sewalt@gmail.com>
2026-07-01 20:24:56 +0200

    Add files via upload

--- api/vendor-claude.js
+++ api/vendor-claude.js
@@ -3,7 +3,7 @@ const https = require('https');
 module.exports = async (req, res) => {
   // CORS headers
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Origin', 'https://complianceas-service-prototype.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
